4a772f0c6a11f3e431626ed020ac6ab4a15b28b0
DenisVolhonsky <dvolhonsky@gmail.com>
2023-10-31 11:38:47 +0100

    add first 3 tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,35 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (books) {
+    res.json(books);
+  } else {
+    res.status(404).json({ error: 'Books is not found' });
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    res.json(books[isbn]);
+  } else {
+    res.status(404).json({ error: 'Book is not found' });
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const matchingBooks = Object.values(books).filter(book => book.author === author);
+
+  if (matchingBooks.length > 0) {
+    res.json(matchingBooks);
+  } else {
+    res.status(404).json({ error: 'The books with this author  doesn\'t found' });
+  }
 });
 
 // Get all books based on title
